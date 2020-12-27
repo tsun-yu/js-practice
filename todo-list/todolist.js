1d688a0828e62c72cb847c9824c08f2e8d543684
Wayne <tsun0227@yahoo.com.tw>
2020-12-27 21:28:43 +0800

    complete button

--- todo-list/todolist.js
+++ todo-list/todolist.js
@@ -1,26 +1,45 @@
+//querySelector
 const slr = (name) => document.querySelector(name);
-
 const sendBtn = slr("#send");
 const input = slr("#input");
 const todolist = slr("#todolist");
+const completedBtn = document.querySelectorAll(".completedBtn");
 
+//存放代辦事項arr
 const todos = [];
 
+//新增代辦事項至todos陣列中
 const addTodo = () => {
   // {id:number , text:string, completed:false}
   const todosItems = { id: +new Date(), text: input.value, completed: false };
   todos.unshift(todosItems);
-  // console.log("todosItems.id", todosItems.id);
   input.value = ``;
 };
-const completedToggle = () => {};
+
+//切換是否已完成
+const completedToggle = (id) => {
+  todos.forEach((v) => {
+    v.id == id && (v.completed = !v.completed);
+  });
+};
+
+//呈現畫面
 const display = () => {
   const displayTodolist = todos.map((value) =>
     value.completed
-      ? `<li><del>${value.text}</del><button>completed</button></li>`
-      : `<li>${value.text}<button id="${value.id}">completed</button></li>`
+      ? `<li><del>${value.text}</del> <button class="btn btn-primary completedBtn" id="${value.id}">completed</button><button class="btn btn-primary deleteBtn" id="${value.id}">delete</button></li>`
+      : `<li>${value.text}<button class="btn btn-primary completedBtn" id="${value.id}">completed</button><button class="btn btn-primary deleteBtn" id="${value.id}">delete</button></li>`
   );
+
   todolist.innerHTML = displayTodolist.join("");
+
+  //完成按鈕事件
+  document.querySelectorAll(".completedBtn").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      completedToggle(e.target.id);
+      display();
+    });
+  });
 };
 
 sendBtn.addEventListener("click", () => {
