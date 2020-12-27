ec80fdd35ce9d653122aa22da45d44ee84347903
Wayne <tsun0227@yahoo.com.tw>
2020-12-27 22:27:30 +0800

    delete

--- todo-list/todolist.js
+++ todo-list/todolist.js
@@ -6,7 +6,7 @@ const todolist = slr("#todolist");
 const completedBtn = document.querySelectorAll(".completedBtn");
 
 //存放代辦事項arr
-const todos = [];
+let todos = [];
 
 //新增代辦事項至todos陣列中
 const addTodo = () => {
@@ -17,18 +17,28 @@ const addTodo = () => {
 };
 
 //切換是否已完成
-const completedToggle = (id) => {
+const completedToggle = (id) =>
   todos.forEach((v) => {
-    v.id == id && (v.completed = !v.completed);
+    v.id == +id && (v.completed = !v.completed);
   });
+//刪除代辦事項
+const deleteItem = (id) => {
+  const newTodos = todos.filter((v) => v.id !== +id);
+  todos = [...newTodos];
 };
 
 //呈現畫面
 const display = () => {
   const displayTodolist = todos.map((value) =>
     value.completed
-      ? `<li><del>${value.text}</del> <button class="btn btn-primary completedBtn" id="${value.id}">completed</button><button class="btn btn-primary deleteBtn" id="${value.id}">delete</button></li>`
-      : `<li>${value.text}<button class="btn btn-primary completedBtn" id="${value.id}">completed</button><button class="btn btn-primary deleteBtn" id="${value.id}">delete</button></li>`
+      ? `<li><del>${value.text}</del>
+      <button class="btn btn-primary completedBtn" id="${value.id}">completed</button>
+      <button class="btn btn-primary deleteBtn" id="${value.id}">delete</button>
+      </li>`
+      : `<li>${value.text}
+      <button class="btn btn-primary completedBtn" id="${value.id}">completed</button>
+      <button class="btn btn-primary deleteBtn" id="${value.id}">delete</button>
+      </li>`
   );
 
   todolist.innerHTML = displayTodolist.join("");
@@ -37,6 +47,12 @@ const display = () => {
   document.querySelectorAll(".completedBtn").forEach((element) => {
     element.addEventListener("click", (e) => {
       completedToggle(e.target.id);
+      display();
+    });
+  });
+  document.querySelectorAll(".deleteBtn").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      deleteItem(e.target.id);
       display();
     });
   });
